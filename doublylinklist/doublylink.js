bf01242d955a2c276101e0b7be939c43052927da
aloksingh3112 <aloksingh3112@gmail.com>
2019-03-15 00:48:03 +0530

    added push dll

--- doublylinklist/doublylink.js
+++ doublylinklist/doublylink.js
@@ -12,4 +12,29 @@ class DoublyLinkList{
         this.tail=null;
         this.length=0;
     }
+
+    push(value){
+     let node=new Node(value);
+     if(this.length===0){
+         this.head=node;
+         this.tail=node;
+      
+     }
+     else{
+       this.tail.next=node;
+       node.prev=this.tail;
+       this.tail=node;
+     
+
+
+     }
+     this.length=this.length+1;
+     return this
+    }
 }
+
+var dls=new DoublyLinkList();
+dls.push(10);
+dls.push(30);
+dls.push(40);
+console.log(dls);
