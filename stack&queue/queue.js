e9f9180bf8421a556cf3ccffb958269fbf0d952a
aloksingh3112 <aloksingh3112@gmail.com>
2019-03-27 00:08:07 +0530

    added dqueue

--- stack&queue/queue.js
+++ stack&queue/queue.js
@@ -23,6 +23,22 @@ class Queue{
         return this.size++;
 
     }
+
+    dqueue(){
+        if(!this.first){
+            return null;
+        }
+        else if(this.first==this.last){
+            var temp=this.first;
+            this.first=this.last=null;
+            return temp;
+        }
+        else{
+            var temp=this.first;
+            this.first=temp.next;
+            return temp;
+        }
+    }
 }
 
 var q=new Queue();
@@ -30,3 +46,9 @@ q.enqueue(1);
 q.enqueue(2);
 q.enqueue(3);
 console.log(q);
+q.dqueue();
+console.log("1",q);
+q.dqueue();
+console.log("2",q);
+q.dqueue();
+console.log("3",q);
