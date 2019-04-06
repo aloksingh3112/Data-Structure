7361d392483835f6092c1876729b832559010f35
aloksingh3112 <aloksingh3112@gmail.com>
2019-04-06 16:13:07 +0530

    fix issue

--- bst/bst.js
+++ bst/bst.js
@@ -20,21 +20,35 @@ class BinarySearchTree{
         else
         {
             var current=this.root;
-            if(current.value===value){
-                return undefined;
-            }
-            else if(value<current.value){
-                while(true){
-                    if(current.left==null){
-                        current.left=newNode;
-                        return this;
-                    }
-                    else{
-                        current=current.left;
-                    }
+            
+            while(true){
+                if(current.value==value){
+                    return undefined;
                 }
+               else if(value<current.value){
+                   if(current.left==null){
+                       current.left=newNode;
+                       return this;
+                   }
+                   else{
+                       current=current.left;
+                   }
+               }
+               else{
+                   if(current.right==null){
+                       current.right=newNode;
+                       return this;
+                   }else{
+                       current=current.right;
+                   }
+               }
+
+
+
 
             }
+            
+             
 
         }
     }
@@ -42,4 +56,8 @@ class BinarySearchTree{
 
 var bst=new BinarySearchTree();
 bst.insert(10);
+// console.log(bst);
+bst.insert(6);
+// console.log(bst);
+bst.insert(7);
 console.log(bst);
