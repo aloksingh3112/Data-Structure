86531c9c7c9c0e1999baec6e6b67c40ae63af8b1
aloksingh3112 <aloksingh3112@gmail.com>
2019-04-06 16:47:16 +0530

    added bst find

--- bst/bst.js
+++ bst/bst.js
@@ -1,63 +1,86 @@
-class Node{
-    constructor(value){
-        this.value=value;
-        this.left=null;
-        this.right=null
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null
 
     }
 }
 
-class BinarySearchTree{
-    constructor(){
-        this.root=null;
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
     }
-    insert(value){
-        var newNode=new Node(value);
-        if(!this.root){
-            this.root=newNode;
+    insert(value) {
+        var newNode = new Node(value);
+        if (!this.root) {
+            this.root = newNode;
             return this;
+        } else {
+            var current = this.root;
+
+            while (true) {
+                if (current.value == value) {
+                    return undefined;
+                } else if (value < current.value) {
+                    if (current.left == null) {
+                        current.left = newNode;
+                        return this;
+                    } else {
+                        current = current.left;
+                    }
+                } else {
+                    if (current.right == null) {
+                        current.right = newNode;
+                        return this;
+                    } else {
+                        current = current.right;
+                    }
+                }
+            }
+        }
+    }
+
+
+    find(value){
+        if(!this.root){
+            return false;
         }
         else
         {
             var current=this.root;
-            
-            while(true){
+            while(current){
                 if(current.value==value){
-                    return undefined;
+                    return current;
                 }
-               else if(value<current.value){
-                   if(current.left==null){
-                       current.left=newNode;
-                       return this;
-                   }
-                   else{
-                       current=current.left;
-                   }
-               }
-               else{
-                   if(current.right==null){
-                       current.right=newNode;
-                       return this;
-                   }else{
-                       current=current.right;
-                   }
-               }
-
-
-
+             else if (value<current.value)
+                {
+                   current=current.left;
+                }
+                else if(value>current.value){
+                     current=current.right;
+                }
+                else{
+                    return false;
+                }
+                
 
             }
-            
-             
 
         }
     }
 }
 
-var bst=new BinarySearchTree();
+var bst = new BinarySearchTree();
 bst.insert(10);
 // console.log(bst);
 bst.insert(6);
 // console.log(bst);
 bst.insert(7);
+bst.insert(12);
+bst.insert(15);
+bst.insert(17);
+bst.insert(11);
+bst.insert(9);
 console.log(bst);
+console.log(bst.find(9));
