7b449851ff94f8d2555c73bef5ab1dfbbfe7aba2
aloksingh3112 <aloksingh3112@gmail.com>
2019-04-06 15:54:52 +0530

    added insert

--- bst/bst.js
+++ bst/bst.js
@@ -11,4 +11,35 @@ class BinarySearchTree{
     constructor(){
         this.root=null;
     }
+    insert(value){
+        var newNode=new Node(value);
+        if(!this.root){
+            this.root=newNode;
+            return this;
+        }
+        else
+        {
+            var current=this.root;
+            if(current.value===value){
+                return undefined;
+            }
+            else if(value<current.value){
+                while(true){
+                    if(current.left==null){
+                        current.left=newNode;
+                        return this;
+                    }
+                    else{
+                        current=current.left;
+                    }
+                }
+
+            }
+
+        }
+    }
 }
+
+var bst=new BinarySearchTree();
+bst.insert(10);
+console.log(bst);
